7d7398abfc24aca61d6350868ea26e1242624926
Caleb Tomlin <caleb.w.tomlin@gmail.com>
2023-06-20 20:38:26 -0500

    analytics push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import { Analytics } from '@vercel/analytics/react';
 import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}
+        <Analytics/>
+        </body>
       </html>
     </ClerkProvider>
   );
